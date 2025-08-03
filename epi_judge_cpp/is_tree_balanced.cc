1528dec71d24051f21af338df44c130d72d736b1
Michał Śledź <michalsledz34@gmail.com>
2025-08-03 14:06:02 +0200

    is_tree_balanced.cc

--- epi_judge_cpp/is_tree_balanced.cc
+++ epi_judge_cpp/is_tree_balanced.cc
@@ -1,11 +1,33 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
-bool IsBalanced(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+#include <algorithm>
+
+int height(const unique_ptr<BinaryTreeNode<int>> &tree) {
+  if (!tree) {
+    return -1;
+  }
+
+  int lh = height(tree->left) + 1;
+  if (lh == -1)
+    return -2;
+
+  int rh = height(tree->right) + 1;
+  if (rh == -1)
+    return -2;
+
+  if (abs(lh - rh) > 1) {
+    return -2;
+  }
+
+  return std::max(lh, rh);
 }
 
-int main(int argc, char* argv[]) {
+bool IsBalanced(const unique_ptr<BinaryTreeNode<int>> &tree) {
+  // TODO - you fill in here.
+  return height(tree) != -2;
+}
+
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"tree"};
   return GenericTestMain(args, "is_tree_balanced.cc", "is_tree_balanced.tsv",
