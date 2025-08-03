f807e045e6e26fb09809c9d2f6f2a3f502178741
Michał Śledź <michalsledz34@gmail.com>
2025-08-03 16:05:24 +0200

    lowest_common_ancestor_with_parent.cc

--- epi_judge_cpp/lowest_common_ancestor_with_parent.cc
+++ epi_judge_cpp/lowest_common_ancestor_with_parent.cc
@@ -4,16 +4,46 @@
 #include "test_framework/test_failure.h"
 #include "test_framework/timed_executor.h"
 
-BinaryTreeNode<int>* Lca(const unique_ptr<BinaryTreeNode<int>>& node0,
-                         const unique_ptr<BinaryTreeNode<int>>& node1) {
-  // TODO - you fill in here.
-  return nullptr;
+int depth(const BinaryTreeNode<int> *node) {
+  int depth = 0;
+  while (node->parent) {
+    ++depth;
+    node = node->parent;
+  }
+  return depth;
 }
-int LcaWrapper(TimedExecutor& executor,
-               const unique_ptr<BinaryTreeNode<int>>& tree, int key0,
+
+BinaryTreeNode<int> *Lca(const unique_ptr<BinaryTreeNode<int>> &node0,
+                         const unique_ptr<BinaryTreeNode<int>> &node1) {
+  // TODO - you fill in here.
+  int n0h = depth(node0.get());
+  int n1h = depth(node1.get());
+
+  BinaryTreeNode<int> *n0i = node0.get();
+  BinaryTreeNode<int> *n1i = node1.get();
+
+  while (n0h > n1h) {
+    n0i = n0i->parent;
+    n0h--;
+  }
+
+  while (n1h > n0h) {
+    n1i = n1i->parent;
+    n1h--;
+  }
+
+  while (n0i != n1i) {
+    n0i = n0i->parent;
+    n1i = n1i->parent;
+  }
+
+  return n0i;
+}
+int LcaWrapper(TimedExecutor &executor,
+               const unique_ptr<BinaryTreeNode<int>> &tree, int key0,
                int key1) {
-  const unique_ptr<BinaryTreeNode<int>>& node0 = MustFindNode(tree, key0);
-  const unique_ptr<BinaryTreeNode<int>>& node1 = MustFindNode(tree, key1);
+  const unique_ptr<BinaryTreeNode<int>> &node0 = MustFindNode(tree, key0);
+  const unique_ptr<BinaryTreeNode<int>> &node1 = MustFindNode(tree, key1);
 
   auto result = executor.Run([&] { return Lca(node0, node1); });
 
@@ -23,7 +53,7 @@ int LcaWrapper(TimedExecutor& executor,
   return result->data;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "tree", "key0", "key1"};
   return GenericTestMain(args, "lowest_common_ancestor_with_parent.cc",
