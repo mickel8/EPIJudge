35110dafb09e6ae96adc99a6e1b51011367c2d4e
Michał Śledź <michalsledz34@gmail.com>
2025-08-03 14:44:20 +0200

    is_tree_symmetric.cc

--- epi_judge_cpp/is_tree_symmetric.cc
+++ epi_judge_cpp/is_tree_symmetric.cc
@@ -1,12 +1,26 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
 
-bool IsSymmetric(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+bool IsSymmetricPair(const unique_ptr<BinaryTreeNode<int>> &t1,
+                     const unique_ptr<BinaryTreeNode<int>> &t2) {
+  if (t1 == nullptr && t2 == nullptr) {
+    return true;
+  }
+
+  if (t1 == nullptr || t2 == nullptr) {
+    return false;
+  }
+
+  return t1->data == t2->data && IsSymmetricPair(t1->left, t2->right) &&
+         IsSymmetricPair(t1->right, t2->left);
 }
 
-int main(int argc, char* argv[]) {
+bool IsSymmetric(const unique_ptr<BinaryTreeNode<int>> &tree) {
+  // TODO - you fill in here.
+  return tree == nullptr || IsSymmetricPair(tree->left, tree->right);
+}
+
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"tree"};
   return GenericTestMain(args, "is_tree_symmetric.cc", "is_tree_symmetric.tsv",
