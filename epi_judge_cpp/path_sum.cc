089e7412dcb43b1ed699c82c44ed5e94d55c93cd
Michał Śledź <michalsledz34@gmail.com>
2025-08-05 10:29:59 +0200

    path_sum.cc

--- epi_judge_cpp/path_sum.cc
+++ epi_judge_cpp/path_sum.cc
@@ -4,13 +4,23 @@
 #include "test_framework/generic_test.h"
 using std::unique_ptr;
 
-bool HasPathSum(const unique_ptr<BinaryTreeNode<int>>& tree,
+bool HasPathSum(const unique_ptr<BinaryTreeNode<int>> &tree,
                 int remaining_weight) {
   // TODO - you fill in here.
-  return true;
+
+  if (tree == nullptr) {
+    return false;
+  }
+
+  if (tree->left == nullptr && tree->right == nullptr) {
+    return (remaining_weight - tree->data) == 0;
+  }
+
+  return HasPathSum(tree->left, remaining_weight - tree->data) ||
+         HasPathSum(tree->right, remaining_weight - tree->data);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"tree", "remaining_weight"};
   return GenericTestMain(args, "path_sum.cc", "path_sum.tsv", &HasPathSum,
--- problem_mapping.js
+++ problem_mapping.js
@@ -1072,7 +1072,7 @@ problem_mapping = {
         },
         "9.06 Find a root to leaf path with specified sum": {
             "C++: path_sum.cc": {
-                "passed": 0,
+                "passed": 7690,
                 "total": 7690
             },
             "Java: PathSum.java": {
